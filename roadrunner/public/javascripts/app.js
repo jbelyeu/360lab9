5b1d33e36c0694df4d46d5790c6ee1299a1d7c65
jbelyeu <jrbelyeu.gmail.com>
2015-04-04 04:07:37 +0000

    built prelim  route for saving new user login information

--- roadrunner/public/javascripts/app.js
+++ roadrunner/public/javascripts/app.js
@@ -1,13 +1,71 @@
-var rrWebsiteApp = angular.module('rrWebsiteApp',
-['map', 'sidebar', 'ngResource', 'ngGrid', 'ui.bootstrap']);
+angular.module('rrWebsiteApp',['ui.router', 'ngResource'])
 
-rrWebsiteApp.config(['$routeProvider', '$locationProvider', 
-function($routeProvider, $locationProvider) 
+.factory('loginFactory', ['$http', function($http)
 {
-	$routeProvider.when('Index', 
-	{
-		templateUrl: 'home',
-		controller: 'MainCtrl'
-	});
-}]);
+	var login = {
+	firstname: '',
+		lastname: '',
+		username: '',
+		password: ''
+	};
 
+	login.signup = function(userData)
+	{
+		return $http.post('/signup/', userData)
+			.success(function(data)
+			{});
+	};
+	return login;
+
+}])
+
+.config
+([
+	'$stateProvider',
+	'$urlRouterProvider', 
+	function($stateProvider, $urlRouterProvider) 
+	{
+		$stateProvider
+		.state('home', 
+		{
+			url: '/home',
+			templateUrl: '/index.ejs',
+			controller: 'MainCtrl'
+		})
+		.state("Login",
+		{
+			url: '/Login',
+			templateUrl: '/login',
+			controller: 'LoginCtrl'
+		});
+		$urlRouterProvider.otherwise('home');
+	}
+])
+
+.controller('LoginCtrl',
+[
+	'$scope',
+	'$stateParams',
+	'loginFactory', 
+	function ($scope, $stateParams, loginFactory)
+	{
+		$scope.addNewUser = function()
+		{
+			if (typeof $scope.firstname  === 'undefined' || 
+				typeof $scope.lastname  === 'undefined' ||
+				typeof $scope.username  === 'undefined' ||
+				typeof $scope.password  === 'undefined') 
+			{
+				return;
+			}
+			var newUser = { 
+                firstname: $scope.firstname,
+                lastname: $scope.lastname,
+                username: $scope.username,
+                password: $scope.password
+            };
+			
+			loginFactory.signup(newUser);
+		};
+	}
+])
